74e971a367b85251cec0f0326431d061d3c5f718
Partho Rahman <partho.rahman@sjinnovation.com>
2022-05-13 15:01:34 +0600

    hook + state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   const names = ['Rahman', 'Akram', 'Mojo']
   return (
     <div className="App">
-      <Abc name={names[0]} words="20"></Abc>
-      <Abc name="Musfiq" words="40"></Abc>
-      <Abc name={names[2]} words="50"></Abc>
-      <Abc></Abc>
+      <div>
+        <MovieCounter></MovieCounter>
+      </div>
+
+      <div>
+        <Abc name={names[0]} words="20"></Abc>
+        <Abc name="Musfiq" words="40"></Abc>
+        <Abc name={names[2]} words="50"></Abc>
+        <Abc></Abc>
+      </div>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
@@ -27,6 +35,25 @@ function Abc(props) {
       <p>I am normal text with "{props.words || 10}" words</p>
     </div>
   )
+}
+
+function MovieCounter() {
+  const [count, setCount] = useState(0);
+  
+  const movieHandler = () => setCount(count + 1);
+
+  return (
+    <div>
+      <button onClick={movieHandler}>Add Movie</button>
+      <h3>Total movie watched : {count}</h3>
+      <AnotherMovieCounter movie={count}></AnotherMovieCounter>
+      <AnotherMovieCounter movie={count}></AnotherMovieCounter>
+    </div>
+  )
+}
+
+function AnotherMovieCounter(props) {
+  return <h4> Movies I acted: {props.movie} </h4>
 }
 
 export default App;
