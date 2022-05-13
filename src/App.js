321a6fd2709a47dfc21b0428c2602a54d66158ce
Partho Rahman <partho.rahman@sjinnovation.com>
2022-05-13 13:22:20 +0600

    props + component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const names = ['Rahman', 'Akram', 'Mojo']
   return (
     <div className="App">
+      <Abc name={names[0]} words="20"></Abc>
+      <Abc name="Musfiq" words="40"></Abc>
+      <Abc name={names[2]} words="50"></Abc>
+      <Abc></Abc>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
+}
+
+function Abc(props) {
+  const abcStyle = {
+    border: '2px solid red',
+    margin: '5px' 
+  }
+  return (
+    <div style={abcStyle}>
+      <h3>I am ABC-{props.name}</h3>
+      <p>I am normal text with "{props.words || 10}" words</p>
+    </div>
+  )
 }
 
 export default App;
